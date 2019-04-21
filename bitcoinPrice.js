2fdc731bc26b5dadd4ee19038d57dd6170db578b
Aaron Iniguez <aaron.iniguez@vrtcal.com>
2019-04-20 17:17:52 -0700

    Feature: show pnl

--- bitcoinPrice.js
+++ bitcoinPrice.js
@@ -2,6 +2,8 @@ const request = require("request");
 const imessage = require("osa-imessage");
 const aaron = "+19165178775";
 const bitcoinUrl = "https://api.coindesk.com/v1/bpi/currentprice.json"
+const initialPrice = 5041;
+//hmmm
 request(bitcoinUrl, function(error, response, body){
     if(!error){
         try{
@@ -10,7 +12,9 @@ request(bitcoinUrl, function(error, response, body){
         catch(err){
             console.log("Error: couldnt read json");
         }
-        imessage.send(aaron, obj.bpi.USD.rate);
+        var currentVal = parseFloat(obj.bpi.USD.rate.replace(",",""));
+        var profit = Math.round(currentVal/initialPrice * 2570 - 2570); 
+        imessage.send(aaron, obj.bpi.USD.rate + "\nprofit: "+profit);
     }
     else{
         console.log("error:", error);
